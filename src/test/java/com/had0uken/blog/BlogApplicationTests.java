a3354ce36133821e5fe233098a8694ade3ba2bde
Serhii <had0uken@ukr.net>
2023-07-24 16:38:53 +0300

    Create new project

---
+++ src/test/java/com/had0uken/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.had0uken.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
